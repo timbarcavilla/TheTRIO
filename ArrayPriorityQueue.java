183697ed37883e4bc6182f4c0c08e434d58b10fe
ArifRoktim <aroktim@stuy.edu>
2017-04-19 21:54:30 -0400

    Implements ArrayPriorityQueue

--- ArrayPriorityQueue.java
+++ ArrayPriorityQueue.java
@@ -5,29 +5,74 @@ HW #32 Getting Past the Velvet Rope
 2017-4-19
 */
 
+/* Implementation details:
+ *  
+ */
+import java.util.ArrayList;
 public class ArrayPriorityQueue implements PriorityQueue{
 
     private ArrayList<Integer> _queue;
 
     public ArrayPriorityQueue(){
-	_queue = new ArrayList<Integer>();
+        _queue = new ArrayList<Integer>();
     }
 
+    // Add item to end of ArrayList
     public void add(Integer newInt){
-	//Your implementation here
+        _queue.add( newInt );
     }
 
     public boolean isEmpty(){ return _queue.size() == 0; }
 
+    //
     public Integer peekMin(){
-	//Your implementation here
+        int min = _queue.get( 0 );
+        for( int i: _queue ){
+            if( i < min ){
+                min = i;
+            }
+        }
+        return min;
     }
 
     public Integer removeMin(){
-	//Your implementation here
+        int minPos = 0;
+        for( int i = 0; i < _queue.size(); i++ ){
+            if( _queue.get( i ) < _queue.get( minPos ) ){
+                minPos = i;
+            }
+            if( _queue.get( minPos ) == 0 ){ break; }
+        }
+        return _queue.remove( minPos );
+    }
+
+    public String toString(){
+        String retStr = "";
+        for( int i: _queue ){
+            retStr += i + " ";
+        }
+        return retStr;
     }
 
     public static void main( String[] args){
-	//Your test cases here
+        ArrayPriorityQueue bob = new ArrayPriorityQueue();
+        System.out.println( "Enqueueing people..." );
+        bob.add( 0 );
+        bob.add( 1 );
+        bob.add( 0 );
+        bob.add( 2 );
+        bob.add( 0 );
+        bob.add( 1 );
+        bob.add( 0 );
+        bob.add( 3 );
+        bob.add( 1 );
+        System.out.println( bob );
+        while( ! bob.isEmpty() ){
+            System.out.println( "Queue: " + bob );
+            System.out.println( "Minimum value: " + bob.peekMin() );
+            System.out.println( "Removed: " + bob.removeMin() );
+            System.out.println();
+        }
     }//end main
+
 }//end class ArrayPriorityQueue
