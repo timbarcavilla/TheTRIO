6c2beb60855c87fc91a66c8e008d40c621690276
timbarcavilla <zwang6@stuy.edu>
2017-04-19 20:14:38 -0400

    skeleton

---
+++ ArrayPriorityQueue.java
@@ -0,0 +1,33 @@
+/*
+Team INSERT NAME HERE- Arif Rotkim, Tim Wang, Allan Wang
+APCS2 PD4
+HW #32 Getting Past the Velvet Rope
+2017-4-19
+*/
+
+public class ArrayPriorityQueue implements PriorityQueue{
+
+    private ArrayList<Integer> _queue;
+
+    public ArrayPriorityQueue(){
+	_queue = new ArrayList<Integer>();
+    }
+
+    public void add(Integer newInt){
+	//Your implementation here
+    }
+
+    public boolean isEmpty(){ return _queue.size() == 0; }
+
+    public Integer peekMin(){
+	//Your implementation here
+    }
+
+    public Integer removeMin(){
+	//Your implementation here
+    }
+
+    public static void main( String[] args){
+	//Your test cases here
+    }//end main
+}//end class ArrayPriorityQueue
