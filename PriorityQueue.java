f1281316ffa5d3da8483449870e7279347613aac
timbarcavilla <zwang6@stuy.edu>
2017-04-19 20:21:31 -0400

    I N T E R F A C E

---
+++ PriorityQueue.java
@@ -0,0 +1,11 @@
+public interface PriorityQueue{
+
+    public void add(Integer x);
+
+    public boolean isEmpty();
+
+    public Integer peekMin();
+
+    public Integer removeMin();
+
+}//ends interface PriorityQueue
